dcab9df96cf5aab4e3af77eb0ad2cd77a9bab394
MajedAbdullah5 <44294277+MajedAbdullah5@users.noreply.github.com>
2020-10-31 16:49:42 +0600

    Components created in the home directories

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/assets/css/style.css';
+import '../src/assets/css/fontawesome.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
