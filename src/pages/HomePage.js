b49f0aa5c4b43f8ecfbeedf8679b54c29f2fa2aa
MajedAbdullah5 <44294277+MajedAbdullah5@users.noreply.github.com>
2020-11-03 22:05:02 +0600

    Footer content added for mobile version

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,6 +8,7 @@ import NavMenuDesktop from "../components/common/NavMenuDesktop";
 import NavMenuMobile from "../components/common/NavMenuMobile";
 import HomeTopMobile from "../components/home/HomeTopMobile";
 import FooterDesktop from "../components/common/FooterDesktop";
+import FooterMobile from "../components/common/FooterMobile";
 
 class HomePage extends Component {
     render() {
@@ -27,7 +28,13 @@ class HomePage extends Component {
                 <FeaturedProducts/>
                 <Collection/>
                 <Categories/>
-                <FooterDesktop/>
+
+                <div className="Desktop">
+                    <FooterDesktop/>
+                </div>
+                <div className="Mobile">
+                    <FooterMobile/>
+                </div>
             </Fragment>
         );
     }
